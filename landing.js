1a28ce137dcc547267b0d76f3d8fecc2243a6d54
Alton <smithiialton@gmail.com>
2024-09-03 23:20:31 -0700

    created folders for each task and moved those files into them

--- landing.js
+++ landing.js
@@ -1,5 +1,13 @@
-function pikaSound(){
-    const sound = document.getElementById("pikaSound")
-
-    sound.play()
+function pikaSound() {
+    const sound = document.getElementById("pikaSound");
+    if (sound) {
+        sound.play().then(() => {
+            // Delay navigation by 1 second
+            setTimeout(() => {
+                window.location.href = "home.html";
+            }, 2000);
+        }).catch(error => console.log("Error playing sound:", error));
+    } else {
+        console.log("Sound element not found.");
+    }
 }
